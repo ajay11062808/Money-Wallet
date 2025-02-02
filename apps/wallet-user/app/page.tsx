21047f4c90f3eabfa5bc191ba313abc4e28d1816
ajay11062808 <jagarlamudiajay1106@gmail.com>
2025-02-02 15:48:10 +0530

    second

---
+++ apps/wallet-user/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@repo/ui/button";
+
+export default function Page() {
+  return <div className="text-2xl">Page
+  <Button className="bg-red-700" appName="Mybutton">Click me</Button>
+  </div>
+
+  
+}
